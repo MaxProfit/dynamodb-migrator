7313d7d9cd139279c72a494d31515632bbb47233
Matthew Williams <mattwilliams1515@gmail.com>
2019-12-24 11:02:47 -0600

    Add constants file

--- cli_logic.py
+++ cli_logic.py
@@ -1,24 +1,30 @@
 import click
+import dynamo_migrate
+import constant
 
 @click.command()
-@click.option('-v', '--verbose', is_flag=True, 
-              help="Will print verbose messages.")
-@click.option('--feed-aws-creds', is_flag=True, 
-              help="Feed in aws credentials instead of system values.")
-@click.option('--using-temp-creds', is_flag=True,
-              help='Using temp credentials instead of permanent access keys')
+@click.option(constant.VERBOSE_FLAG_SHORT, constant.VERBOSE_FLAG_LONG, 
+              is_flag=True, help=constant.VERBOSE_HELP)
+@click.option(constant.FEED_AWS_CREDENTIALS_FLAG, is_flag=True, 
+              help=constant.FEED_AWS_CREDENTIALS_HELP)
+@click.option(constant.USING_TEMP_CREDENTIALS_FLAG, is_flag=True,
+              help=constant.USING_TEMP_CREDENTIALS_HELP)
 def main(verbose, feed_aws_creds, using_temp_creds):
     """Simple program that greets NAME for a total of COUNT times."""
 
-    if feed_aws_credentials:
-        string = click.prompt("Access Key ID", type=str)
-        string2 = click.prompt("Secret Access Key", type=str)
+    if feed_aws_creds:
+        string = click.prompt(constant.ACCESS_KEY_PROMPT, type=str)
+        string2 = click.prompt(constant.SECRET_ACCESS_KEY_PROMPT, type=str)
         if using_temp_creds:
-            string3 = click.prompt("Session ID", type=str)
+            string3 = click.prompt(constant.SESSION_ID_PROMPT, type=str)
         click.echo("So you're access key is {} huh".format(string))
         click.echo("So you're access key is {} huh".format(string2))
     if verbose:
         click.echo("so I hear you're from {}".format("Mathewistan"))
+
+
+    ckl = DynamoMigrator(thing, selp)
+    ckl.myFunc(list_of_secondary_index_tuples)
 
 if __name__ == '__main__':
     main()
--- dynamo_migrate.py
+++ dynamo_migrate.py
@@ -1,7 +1,29 @@
 import boto3
+import constant
 
-client = boto3.client(
-    'dynamodb',
-    aws_access_key_id='AKIAIO5FODNN7EXAMPLE',
-    aws_secret_access_key='ABCDEF+c2L7yXeGvUyrPgYsDnWRRC1AYEXAMPLE'
-)
+class DynamoMigrator:
+
+    def __init__(self, old_table_name, new_table_name, access_key=None, access_key_secret=None):
+        self.old_table_name = old_table_name
+        self.new_table_name = new_table_name
+        self.client = __create_dynamo_client(access_key, access_key_secret)
+
+    def __create_dynamo_client(self, access_key, access_key_secret):
+        if not access_key is None and not access_key_secret is None:
+            return boto3.client(constant.DYNAMODB)
+        else:
+            return boto3.client(constant.DYNAMODB,
+                                aws_access_key_id=access_key,
+                                aws_secret_access_key=access_key_secret)
+    def myFunc(self):
+        print("Hello my name is {}".format(self.name))
+
+    def create_new_table(self):
+        pass
+
+
+# Old table name
+
+# New table name
+# Get the local indexes
+# Get the global indexes
